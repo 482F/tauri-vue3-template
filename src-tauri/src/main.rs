e35014abdb0fce56820af311bdaf31f23f036725
482F <482F.git.mail@482f.net>
2023-01-29 00:00:00 +0900

    二個目のインスタンスの引数をもらえるように

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,12 @@ use tauri::Manager;
 
 mod commands;
 
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  argv: Vec<String>,
+  cwd: String,
+}
+
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
@@ -15,6 +21,14 @@ fn main() {
       set_shadow(&main_window, true).unwrap();
       Ok(())
     })
+    .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+      let main_window = app.get_window("main").unwrap();
+      main_window.show();
+      main_window.unminimize();
+      main_window.set_focus();
+
+      main_window.emit("second-instance", Payload { argv, cwd }).unwrap();
+    }))
     .invoke_handler(tauri::generate_handler![commands::update_msg])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
