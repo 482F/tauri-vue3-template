02200d719c00f5d9e9c79792b5ca6c1bf548cce4
482F <482F.git.mail@482f.net>
2023-01-30 00:00:00 +0900

    rust 側との通信のサンプルを作成

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <SubComponent v-model:msg="msg" />
+</template>
+
+<script setup lang="ts">
+import SubComponent from './SubComponent.vue'
+
+import { ref } from 'vue'
+const msg = ref('initial msg')
+</script>
