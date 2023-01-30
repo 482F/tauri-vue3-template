1071fb5bea738e34a0f5e0426591199223a2bc26
482F <482F.git.mail@482f.net>
2023-01-31 00:00:00 +0900

    二個目のインスタンスの引数をもらえるように

--- src/components/SubComponent.vue
+++ src/components/SubComponent.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>{{ msg }}</h1>
     <v-btn @click="updateMsg">{{ count }}</v-btn>
+    <div>{{ secondInstancePayload }}</div>
     <div>line</div>
     <div>line</div>
     <div>line</div>
@@ -33,6 +34,21 @@
 <script setup lang="ts">
 import { invoke } from '@tauri-apps/api'
 import { ref } from 'vue'
+import { listen } from '@tauri-apps/api/event'
+
+type EventPayload = {
+  argv: string[]
+  cwd: string
+}
+const secondInstancePayload = ref<EventPayload>({
+  argv: [],
+  cwd: '',
+})
+
+listen('second-instance', (e: { payload: EventPayload }) => {
+  console.log('second-instance', { e })
+  secondInstancePayload.value = e.payload
+})
 
 const props = withDefaults(
   defineProps<{ msg: string; titles: { left: string; right: string } }>(),
